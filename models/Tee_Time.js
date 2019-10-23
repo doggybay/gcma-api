e893578bceb460dcf90b4b830de1c97e4f960080
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-23 15:10:24 -0700

    updated name

---
+++ models/Tee_Time.js
@@ -0,0 +1,40 @@
+const { Model } = require('objection')
+
+class Tee_Time extends Model {
+  static get tableName() {
+    return 'tee_times'
+  }
+
+  static get relationMappings() {
+    const Customer = require('./Customer')
+    return {
+      customers: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Customer,
+        join: {
+          from: 'tee_times.id',
+          through: {
+            from: 'customers_tee_times.tee_time_id',
+            to: 'customers_tee_times.tee_time_id',
+          },
+          to: 'customers.id'
+        }
+      }
+    }
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['time'],
+
+      properties: {
+        id: { type: 'integer' },
+        time: { type: 'string' }
+      }
+    }
+  }
+
+}
+
+module.exports = Tee_Time
