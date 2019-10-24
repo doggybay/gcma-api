a3a70cb82054b4b06495619160ba9122d616ba60
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-24 13:08:18 -0700

    updated customer controller

--- controllers/customers.js
+++ controllers/customers.js
@@ -3,3 +3,15 @@ const Customer = require('../models/Customer')
 exports.getAllCustomers = (req, res) => {
   Customer.query().eager('tee_times').then(customers => res.json(customers))
 }
+
+exports.getOneCustomer = (req, res) => {
+  Customer.query().findById(req.params.id).eager('tee_times').then(customer => res.json(customer))
+}
+
+exports.updateOneCustomer = (req, res) => {
+  Customer.query().findById(req.params.id).patch(req.body).returning('*').then(updatedCustomer => res.json(updatedCustomer))
+}
+
+exports.deleteOneCustomer = (req, res) => {
+  Customer.query().deleteById(req.params.id).returning('*').then(deletedCustomer => res.json(deletedCustomer))
+}
