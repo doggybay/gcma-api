97d132ef60e151f609a8ae4bdd6e8226c8e070c8
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-30 12:51:59 -0700

    changed time to string type

--- db/migrations/02_tee_times.js
+++ db/migrations/02_tee_times.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('tee_times', table => {
     table.increments();
-    table.datetime('time').notNullable();
+    table.string('time').notNullable();
   })
 };
 
