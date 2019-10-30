07663e6904b2e85cd3ef1a861119222984632f06
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-30 13:12:26 -0700

    added tee times routes

--- routes/tee_times.js
+++ routes/tee_times.js
@@ -4,6 +4,9 @@ const teeTimesController = require('../controllers/tee_times')
 
 router.get('/tee-times', teeTimesController.getAllTeeTimes)
 router.get('/tee-times/:id', teeTimesController.getOneTeeTime)
+router.post('/tee-times', teeTimesController.addOneTeeTime)
+router.patch('/tee-times/:id', teeTimesController.updateOneTeeTime)
+router.delete('/tee-times/:id', teeTimesController.deleteOneTeeTime)
 
 
 module.exports = router
